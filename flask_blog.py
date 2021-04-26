da48092a7c79670d3b357a0f9a99d5701d9ce274
economicactivist <mountainscott67@yahoo.com>
2021-04-25 22:04:57 -0700

    login form validates but doesn't return either flash message

--- flask_blog.py
+++ flask_blog.py
@@ -1,14 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)  #so flask knows where to look for templates and static files
+
+app.config['SECRET_KEY'] ='fdc03f61e31fac890e1e89617fabac1e'  #make an environment variable later
+# (in terminal)
+# import secrets
+# secrets.token_hex(16)  16 is the number of bytes
+
+posts = [{"title":"first post", "author": "bob", "content": "my first post", "date_posted":"Apr 22, 2021"},
+{"title":"second post", "author": "david", "content": "my second post", "date_posted":"Apr 23, 2021"}]
+
 
 @app.route('/')  #decorators add functionality to existing functions
 @app.route('/home') 
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')  
 def about():
-    return '<h1>About us</h1>'
+    return render_template('about.html', title="About")
+
+@app.route('/register', methods=['GET', 'POST'])  
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # the 'success' string refers to the bootstrap class
+        flash(f'Account created for {form.username.data}!', category='success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title="Register", form=form)
+
+@app.route('/login', methods=['GET', 'POST'])  
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been successfully logged in!', category='success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password', category='danger')
+    return render_template('login.html', title="Login", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo, Email, InputRequired #https://wtforms.readthedocs.io/en/2.3.x/validators/
+# from email_validator import validate_email, EmailNotValidError #https://github.com/JoshData/python-email-validator
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    # try:
+    email = StringField('Email', validators=[InputRequired("Please enter your email address."), Email("This field requires a valid email address")])
+    #     valid = validate_email(email)
+    #     email = valid.email
+    # except EmailNotValidError as e:
+    #     print(str(e))
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message="Passwords must match")]) #diff from video
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[InputRequired("Please enter your email address."), Email("This field requires a valid email address")])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
