b737ebb3d3e2c73dc8e4258e52bb1f2990c066b0
economicactivist <mountainscott67@yahoo.com>
2021-05-13 21:28:01 -0700

    notes about using 'backref' in User class

--- flask_blog.py
+++ flask_blog.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # make an environment variable later
 app.config['SECRET_KEY'] = 'fdc03f61e31fac890e1e89617fabac1e'
-# /// means relative path from current file (i.o.w, same directory)
+# "///" means relative path from current file (i.o.w, same directory)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)  # database instance
@@ -29,10 +29,42 @@ class User(db.Model):
     # password will be hashed to a 60-character string
     # unique not true because two different users can have the same password
     posts = db.relationship('Post', backref="author", lazy=True)
-    # lazy=True is like a normal select statement
+    # 'Post' is capitalized because the Post class is being referenced
+    # lazy=True is like a normal select statement that grabs the whole table at once
     # backref is a shortcut that prevents having to place an explict back_populates
     # on both models/tables.  However, this is confusing because the other table doesn't
     # have an author field.  Schafer says it's like creating a new column in the Post model
+    # posts is not a column but rather an attribute of the class.  it runs a query on a specific
+    # instance of the User class and returns a list of post instances associated with that instance
+    # (every instance will be associated with a specific user_id)
+
+    """ ***Example***
+        user_1 = User(username="David", email="david@yahoo.com", password="password")
+        user_2 = User(...)
+        db.session.add(user_1)
+        db.session.commit()
+        [other commands: User.query.all(); User.query.first(); User.query.filter_by()]
+        user = User.query.filter_by(username="Corey").first()  #use first() so that it's not a list
+        #could also say "user = User.query.get(1)
+        [now you can do things on the instance like "user.id"]
+        ***to the point***
+        now you can say "user.posts" to run a query on User('David', 'david@yahoo.com', 'default.jpg')
+        1) user.posts returns an empty list
+        2) post_1 = Post(title="First post", content="My First Post", user_id=user.id) #note user.id not user_id
+           post_2 = Post(title="Second post", content="My Second Post", user_id=user.id)
+        3) db.session.add(post_1)
+           db.session.add(post_2)
+           db.session.commit()
+        4) user.posts returns [Post('First Post', '2021-xx-xx ....'), Post('Second Post', '2021-xx-xx ....')]
+        5) post = Post.query.first()
+        6) post.user_id   returns 1
+        ***important***
+        7) post.author returns User('David', 'david@yahoo.com', 'default.jpg')
+        so using backref="author" allows a post instance to hook into the associated user instance 
+        and return it. (You can also think of it as creating an invisible extra column in the posts table 
+        that holds the user instance)
+        
+    """
 
     def __repr__(self):
         return f'User("{self.username}", "{self.email}", "{self.image_file}")'
@@ -48,6 +80,8 @@ class Post(db.Model):
     # in the function, not the current time, as the argument
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # 'user.id' is lowercase because it is referencing the table name and
+    # column name (not the User class)
 
     def __repr__(self):
         return f'Post("{self.title}", "{self.date_posted}")'
